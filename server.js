d3ab5b15d2149be2ef84760608ab3cbf86e95be2
nhahn <nphahn@gmail.com>
2014-07-28 18:28:54 -0400

    Updated the runner script to remove the NPM variables

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 var spawn = require('child_process').spawn;
 var dir = process.cwd();
 
+//Remove all of the stupid NPM variables -- they mess everything up
+tmpEnv = {};
+for (env in process.env) { 
+  if (!env.match(/^npm/))
+  {
+    tmpEnv[env] = process.env[env];
+  }
+}
+
 if (process.env.OPENSHIFT_REPO_DIR != undefined) {
   dir = process.env.OPENSHIFT_REPO_DIR;
 }
 var fullPath = dir + '/node_modules/ep_etherpad-lite/node/server.js';
 
 var spawn = require('child_process').spawn,
-    ls    = spawn('node', [fullPath]);
+    ls    = spawn('node', [fullPath], {env: tmpEnv});
 
 ls.stdout.on('data', function (data) {
   console.log(data.toString().trim());
